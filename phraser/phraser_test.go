05e349ca01eac7719015f18400c64a66537f4e84
Larry Hosken <lahosken@gmail.com>
2016-04-20 15:52:48 -0700

    First pass at ngrams. Might take a few days(!) to download Ngrams, so why wait?

--- phraser/phraser_test.go
+++ phraser/phraser_test.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-//  "strings"
-  "testing"
+	//  "strings"
+	"testing"
 )
 
 func TestTokenizeTypical(t *testing.T) {
-  tokens := tokenize("Is Omotic Afro-Asiatic?")
-  expected := []string{"is", "omotic", "afro", "asiatic"}
-  if len(tokens) != len(expected) {
-    t.Errorf("tokenize() returned strange value %v ", tokens)
-    return
-  }
-  for ix, _ := range expected {
-    if tokens[ix] != expected[ix] {
-      t.Errorf("tokenize() returned strange value %v", tokens)
-      return
-    }
-  }
+	tokens := tokenize("Is Omotic Afro-Asiatic?")
+	expected := []string{"is", "omotic", "afro", "asiatic"}
+	if len(tokens) != len(expected) {
+		t.Errorf("tokenize() returned strange value %v ", tokens)
+		return
+	}
+	for ix, _ := range expected {
+		if tokens[ix] != expected[ix] {
+			t.Errorf("tokenize() returned strange value %v", tokens)
+			return
+		}
+	}
 }
 
 func TestTokenizeApostrophe(t *testing.T) {
-  tokens := tokenize("It's a small world, but I wouldn't want to paint it.")
-  expected := []string{"its", "a", "small", "world", "but", "i", "wouldnt", "want", "to", "paint", "it"}
-  if len(tokens) != len(expected) {
-    t.Errorf("tokenize() returned strange value %v ", tokens)
-    return
-  }
-  for ix, _ := range expected {
-    if tokens[ix] != expected[ix] {
-      t.Errorf("tokenize() returned strange value %v", tokens)
-      return
-    }
-  }
+	tokens := tokenize("It's a small world, but I wouldn't want to paint it.")
+	expected := []string{"its", "a", "small", "world", "but", "i", "wouldnt", "want", "to", "paint", "it"}
+	if len(tokens) != len(expected) {
+		t.Errorf("tokenize() returned strange value %v ", tokens)
+		return
+	}
+	for ix, _ := range expected {
+		if tokens[ix] != expected[ix] {
+			t.Errorf("tokenize() returned strange value %v", tokens)
+			return
+		}
+	}
 }
 
 func TestIngestWikiPageRedirect(t *testing.T) {
-  co := counter{}
-  // sample from Twilight Saga wikia. "Vampire Mythology" redirects to Vampire
-  page := `
+	co := counter{}
+	// sample from Twilight Saga wikia. "Vampire Mythology" redirects to Vampire
+	page := `
     <title>Vampire Mythology</title>
     <ns>0</ns>
     <id>1980</id>
@@ -54,22 +54,22 @@ func TestIngestWikiPageRedirect(t *testing.T) {
       <comment>Vampire</comment>
       <text xml:space="preserve" bytes="21">#REDIRECT [[Vampire]]</text>
     </revision>`
-  ingestWikiPage(page, &co)
-  if co.d["vampire"] < 1 {
-    t.Errorf("ingestWikiPage for redir page returned strange value %v", co)
-    return
-  }
-  if co.d["vampire mythology"] < 1 {
-    t.Errorf("ingestWikiPage for redir page returned strange value %v", co)
-    return
-  }
+	ingestWikiPage(page, &co)
+	if co.d["vampire"] < 1 {
+		t.Errorf("ingestWikiPage for redir page returned strange value %v", co)
+		return
+	}
+	if co.d["vampire mythology"] < 1 {
+		t.Errorf("ingestWikiPage for redir page returned strange value %v", co)
+		return
+	}
 }
 
 func TestIngestWikiPageFile(t *testing.T) {
-  co := counter{}
-  // sample from Gossip Girl wikia. This non-text-y .jpg shouldn't boost
-  // phrase counts
-  page := `
+	co := counter{}
+	// sample from Gossip Girl wikia. This non-text-y .jpg shouldn't boost
+	// phrase counts
+	page := `
     <title>File:Dair-Wallpaper-dan-and-blair-1535983-1280-800.jpg</title>
     <ns>6</ns>
     <id>3866</id>
@@ -83,16 +83,16 @@ func TestIngestWikiPageFile(t *testing.T) {
       </contributor>
       <text xml:space="preserve" bytes="0" />
     </revision>`
-  ingestWikiPage(page, &co)
-  if len(co.d) > 0 {
-    t.Errorf("ingestWikiPage for File: page returned strange value %v", co)
-  }
+	ingestWikiPage(page, &co)
+	if len(co.d) > 0 {
+		t.Errorf("ingestWikiPage for File: page returned strange value %v", co)
+	}
 }
 
 func TestIngestWikiPagePisa(t *testing.T) {
-  co := counter{}
-  // thank you Futurama wikia for this sample
-  page := `
+	co := counter{}
+	// thank you Futurama wikia for this sample
+	page := `
     <title>Pisa</title>
     <ns>0</ns>
     <id>47</id>
@@ -126,25 +126,25 @@ In [[3001]], Pisa was one of several cities [[Fry]] and [[Bender]] flew over dur
 [[Category:Cities]]
 [[Category:Locations]]</text>
     </revision>`
-  ingestWikiPage(page, &co)
-  for _, expectedPresent := range []string{
-    "pisa", "city on", "earth", "city on earth", "locations",
-  } {
-    if co.d[expectedPresent] < 1 {
-      t.Errorf("ingestWikiPage for Pisa example expected %s but didn't see it", expectedPresent)
-      return
-    }
-  }
-  for _, expectedAbsent := range []string { // don't let these sneak in
-    "quot", "category locations", "ref", "references",
-  } {
-    if co.d[expectedAbsent] != 0 {
-      t.Errorf("ingestWikiPage for Pisa example should have ignored %s but alas counted it", expectedAbsent)
-      return
-    }
-  }
-  if co.d["pisa"] <= co.d["city on"] {
-      t.Errorf(`ingestWikiPage for Pisa example should have scored "pisa" higher than "city on" but didn't.`)
-      return    
-  }
+	ingestWikiPage(page, &co)
+	for _, expectedPresent := range []string{
+		"pisa", "city on", "earth", "city on earth", "locations",
+	} {
+		if co.d[expectedPresent] < 1 {
+			t.Errorf("ingestWikiPage for Pisa example expected %s but didn't see it", expectedPresent)
+			return
+		}
+	}
+	for _, expectedAbsent := range []string{ // don't let these sneak in
+		"quot", "category locations", "ref", "references",
+	} {
+		if co.d[expectedAbsent] != 0 {
+			t.Errorf("ingestWikiPage for Pisa example should have ignored %s but alas counted it", expectedAbsent)
+			return
+		}
+	}
+	if co.d["pisa"] <= co.d["city on"] {
+		t.Errorf(`ingestWikiPage for Pisa example should have scored "pisa" higher than "city on" but didn't.`)
+		return
+	}
 }
